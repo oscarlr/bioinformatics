af6061c715132ccb0f3a38f47099c2422e3c5e61
Oscar Rodriguez <oscar.rodriguez@icahn.mssm.edu>
2018-12-23 19:09:05 -0500

    edit

--- bash/assemble_fosmids.sh
+++ bash/assemble_fosmids.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+set -e -x
 
 scratch=$1
-work=$2
-bamfile=$3
-bedfile=$4
-ref=$5
+bamfile=$2
+bedfile=$3
+ref=$4
 
 threads=1
 memory=8
@@ -12,29 +12,32 @@ memory=8
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname $(dirname "$SCRIPT"))
 
-blasr \
-    ${bamfile} \
-    ${ref} \
-    --bestn 1 \
-    --bam \
-    --nproc ${threads} \
-    --out ${scratch}/reads_to_ref.bam
+if [ ! -s ${scratch}/reads_to_ref.sorted.bam.bai ]
+then
+    blasr \
+	${bamfile} \
+	${ref} \
+	--bestn 1 \
+	--bam \
+	--nproc ${threads} \
+	--out ${scratch}/reads_to_ref.bam
+    
+    samtools \
+	sort -@ ${threads} \
+	${scratch}/reads_to_ref.bam \
+	-o ${scratch}/reads_to_ref.sorted.bam
+    
+    samtools index ${scratch}/reads_to_ref.sorted.bam
 
-samtools \
-    sort -@ ${threads} \
-    ${scratch}/${sample}/reads_to_ref.bam \
-    -o ${scratch}/${sample}/reads_to_ref.sorted.bam
-
-samtools index ${scratch}/${sample}/reads_to_ref.sorted.bam
-
-rm -f ${scratch}/${sample}/reads_to_ref.bam
+    rm -f ${scratch}/reads_to_ref.bam
+fi
 
 cat ${bedfile} | while read chrom start end sample
 do
     mkdir -p ${scratch}/${sample} 
     
     python ${SCRIPTPATH}/python/extract_reads_from_region.py \
-	${scratch}/${sample}/reads_to_ref.sorted.bam \
+	${scratch}/reads_to_ref.sorted.bam \
         ${chrom} \
         ${start} \
         ${end} > ${scratch}/${sample}/reads.fasta
