d680faa1ade1c538839bcab51c6755addddc6607
Oscar Rodriguez <oscar.rodriguez@icahn.mssm.edu>
2019-01-02 16:28:55 -0500

    edit

--- python/mapping_coords.py
+++ python/mapping_coords.py
@@ -8,5 +8,8 @@ for read in samfile:
     ref = samfile.get_reference_name(read.reference_id)
     start = read.reference_start
     end = read.reference_end
-    out = [ref,start,end]
+    strand = "+"
+    if read.is_reverse:
+        strand = "-"
+    out = [ref,start,end,strand]
     print "%s" % "\t".join(map(str,out))
