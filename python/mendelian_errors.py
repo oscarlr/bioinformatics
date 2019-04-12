0285bf4893d31b467f702e4cfc652bc4cd762c95
Oscar Rodriguez <oscar.rodriguez@icahn.mssm.edu>
2019-04-12 08:37:25 -0400

    add

--- python/mendelian_errors.py
+++ python/mendelian_errors.py
@@ -1,7 +1,9 @@
 #!/bin/env python
 import sys
 
-vcffile = sys.argv[1]
+son_vcffile = sys.argv[1]
+parent1_vcffile = sys.argv[2]
+parent2_vcffile = sys.argv[3]
 
 def is_error(son,parent1,parent2):
     error = False
@@ -18,13 +20,44 @@ def is_error(son,parent1,parent2):
             error = True
     return error
 
-with open(vcffile,'r') as vcffh:
-    for line in vcffh:
-        if line.startswith("#"):
-            continue
-        line = line.rstrip().split('\t')
-        son = line[9].split(":")[0]
-        parent1 = line[10].split(":")[0]
-        parent2 = line[11].split(":")[0]
-        if is_error(son,parent1,parent2):
-            print "\t".join(line)
+def read_genotype(vcffile):
+    genotypes = {}
+    with open(vcffile,'r') as vcffh:
+        for line in vcffh:
+            if line.startswith("#"):
+                continue
+            line = line.rstrip().split('\t')
+            pos = line[1]
+            genotype = line[9].split(":")[0]
+            genotypes[pos] = genotype
+    return genotypes
+
+son_genotype = read_genotype(son_vcffile)
+parent1_genotype = read_genotype(parent1_vcffile)
+parent2_genotype = read_genotype(parent2_vcffile)
+
+number_of_het = 0
+number_of_het_errors = 0
+number_of_hom = 0
+number_of_hom_errors = 0
+for pos in son_genotype:
+    if pos not in parent1_genotype:
+        continue
+    if pos not in parent2_genotype:
+        continue    
+    if son_genotype[pos] == "0/1":
+        number_of_het += 1.0        
+    if son_genotype[pos] == "1/1":
+        number_of_hom += 1.0      
+    if is_error(son_genotype[pos],parent1_genotype[pos],parent2_genotype[pos]):
+        print "error: %s" % pos
+        if son_genotype[pos] == "0/1":
+            number_of_het_errors += 1.0
+        if son_genotype[pos] == "1/1":
+            number_of_hom_errors += 1.0
+
+print number_of_het
+print number_of_hom
+print number_of_het_errors/number_of_het
+print number_of_hom_errors/number_of_hom
+
